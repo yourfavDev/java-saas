0799b026570ffde7f5a0621090ac2df32b23bd30
yourfavDev <istrateeduard@icloud.com>
2025-06-26 01:20:07 +0300

    commit

---
+++ src/main/java/com/libraries/saas/SaasApplication.java
@@ -0,0 +1,13 @@
+package com.libraries.saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaasApplication.class, args);
+    }
+
+}
